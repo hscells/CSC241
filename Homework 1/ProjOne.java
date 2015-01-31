b59d2dcb98c82893a6b0ddbaae61b8d322acdfbe
Harry Scells <harryscells@gmail.com>
2015-01-30 21:23:35 -0500

    updated files

--- Homework 1/ProjOne.java
+++ Homework 1/ProjOne.java
@@ -1,31 +1,57 @@
 
 public class ProjOne{
 
-   public class Room{
+   public static class Room{
 
       private String name;
       private Animal[] animals = new Animal[10];
+      private int num_animals = 0;
 
-      Room(String room_name){
-         name = room_name;
+      Room(String n){
+
+         name = n;
+
       }
 
+
       public void addAnimal(Animal animal){
+
+         if (num_animals < 10){
+            animal.setRoom(this);
+            animals[num_animals] = animal;
+            num_animals++;
+         }
 
       }
 
       public String toString(){
+
          return name;
+
       }
 
    }
 
-   public class Animal{
+   public static class Animal{
 
       private String name;
       private Room room;
 
-      Animal (Room room){
+      Animal (String n){
+
+         name = n;
+
+      }
+
+      public void setRoom(Room r){
+
+         room = r;
+
+      }
+
+      public String toString(){
+
+         return name;
 
       }
 
@@ -33,6 +59,11 @@ public class ProjOne{
 
    public static void main(String[] a){
 
+      Room room = new Room("test");
+      System.out.println(room.toString());
+      Animal pig = new Animal("pig");
+      System.out.println(pig.toString());
+      room.addAnimal(pig);
 
    }
 
