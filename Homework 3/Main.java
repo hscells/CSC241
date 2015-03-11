3c705d9b8cbef5a471b614cd253b372c5fd58450
Harry Scells <harryscells@gmail.com>
2015-03-10 20:55:53 -0400

    input a file name

--- Homework 3/Main.java
+++ Homework 3/Main.java
@@ -1,11 +1,27 @@
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import org.xml.sax.helpers.DefaultHandler;
+import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] a) throws Exception {
-    SAXParserFactory factory = SAXParserFactory.newInstance();
-    SAXParser saxParser = factory.newSAXParser();
-    saxParser.parse("input.xml", new XMLHandler());
+
+   public static void main(String[] a) throws Exception {
+
+      Scanner input = new Scanner(System.in);
+      String input_file = input.next();
+
+      SAXParserFactory factory = SAXParserFactory.newInstance();
+      SAXParser saxParser = factory.newSAXParser();
+      try {
+
+         saxParser.parse(input_file, new XMLHandler());
+
+      } catch (Exception e) {
+
+         System.out.println("The specified file (" + input_file + ") does not exist.");
+
+      }
+
   }
+
 }
--- Homework 3/XMLHandler.java
+++ Homework 3/XMLHandler.java
@@ -2,24 +2,29 @@ import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.Attributes;
 
 public class XMLHandler extends DefaultHandler {
-  public void startDocument() {
-    System.out.println("Document parsing started.");
-  }
 
-  public void endDocument() {
-    System.out.println("Document parsing ended.");
-  }
+   public void startDocument() {
 
-  public void startElement(String uri,
-                String localName,
-                String qName,
-                Attributes attributes) {
-    System.out.println("Starting tag " + qName);
-  }
+      System.out.println("Document parsing started.");
 
-  public void endElement(String uri,
-              String localName,
-              String qName) {
-    System.out.println("Ending tag " + qName);
-  }
+   }
+
+   public void endDocument() {
+
+      System.out.println("Document parsing ended.");
+
+   }
+
+   public void startElement(String uri, String localName, String qName, Attributes attributes) {
+
+      System.out.println("Starting tag " + qName);
+
+   }
+
+   public void endElement(String uri, String localName, String qName) {
+
+      System.out.println("Ending tag " + qName);
+
+   }
+
 }
