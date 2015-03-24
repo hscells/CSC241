bea104117608b72f4425ca9f5da248ad969a9d25
Harry Scells <harryscells@gmail.com>
2015-03-24 13:17:16 -0400

    Give the main class some nice feedback

--- Homework 4/Main.java
+++ Homework 4/Main.java
@@ -33,18 +33,24 @@ public class Main {
       // create a new variable so I don't have to write results.getRooms().get()
       HashMap<String, Room> rooms = results.getRooms();
 
+      // create a nice player object to contain it from the xml
+      PC player = results.getPlayer();
+
       // let the user know what rooms were loaded in
       System.out.println("Rooms found in the input file: ");
       for (String key : rooms.keySet()){
 
-         System.out.println(rooms.get(key).name());
+         System.out.println("\t" + rooms.get(key).name());
 
       }
 
-      // allow the user to query the rooms
-      //System.out.println("Type `q` to quit.");
-      Boolean finished = false;
-      while (!results.getPlayer().play(input));
+      System.out.print("Type your commands here > ");
+
+      while (player.play(input)){
+
+         System.out.print("> ");
+
+      }
 
   }
 
