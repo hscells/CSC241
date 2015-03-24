a2911f7efbed6fcd9a205b6016ce851f01d920a3
Harry Scells <harryscells@gmail.com>
2015-03-24 19:45:29 -0400

    added notify methods, needs bug fixes

--- Homework 4/Animal.java
+++ Homework 4/Animal.java
@@ -1,18 +1,24 @@
+import java.util.Random;
+
 public class Animal extends Creature{
+
+   public void notifyCreature(){
+
+      if(getRoom().getState().equals("clean")){
+
+         Random r = new Random();
+         Room chosen_room;
+         while((chosen_room = getRoom().getRoom(r.nextInt(4))) == null);
+         moveRoom(chosen_room);
+         System.out.println(toString() + " moved to the " + getRoom().name() + " room.");
+
+      }
+
+   }
 
    Animal(String n, String d){
 
       super(n,d);
-
-   }
-
-   public void notifyCreature(){
-
-      if(getRoom().getState().equals("dirty")){
-
-         
-
-      }
 
    }
 
--- Homework 4/NPC.java
+++ Homework 4/NPC.java
@@ -1,6 +1,19 @@
+import java.util.Random;
+
 public class NPC extends Creature{
 
-   public void notifyCreature(String s){
+   public void notifyCreature(){
+
+      if(getRoom().getState().equals("clean")){
+
+         Random r = new Random();
+         Room chosen_room;
+         while((chosen_room = getRoom().getRoom(r.nextInt(4))) == null);
+         moveRoom(chosen_room);
+         System.out.println(toString() + " moved to the " + getRoom().name() + " room.");
+
+
+      }
 
    }
 
