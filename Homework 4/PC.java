4296c1685d55b6e97fea46f9072fa6037da99a99
Harry Scells <harryscells@gmail.com>
2015-03-24 13:17:31 -0400

    templates for things to come

--- Homework 4/PC.java
+++ Homework 4/PC.java
@@ -7,21 +7,57 @@ public class PC extends Creature{
 
    public boolean play(Scanner s){
 
-      if (s.equals("help")){
+      String input = s.next();
+      input = input.toLowerCase();
+
+      if (input.equals("help")){
 
          System.out.println("This is not helpful");
          return true;
 
-      } else if (s.equals("look")){
+      } else if (input.equals("look")){
 
          System.out.println(room.toString());
          return true;
 
-      } else {
+      } else if (input.equals("clean")){
 
+         System.out.println("I am cleaning the room");
+         return true;
+
+      } else if (input.equals("dirty")){
+
+         System.out.println("I am messing up the room");
+         return true;
+
+      } else if (input.equals("north")){
+
+         System.out.println("I move north");
+         return true;
+
+      } else if (input.equals("east")){
+
+         System.out.println("I move east");
+         return true;
+
+      } else if (input.equals("south")){
+
+         System.out.println("I move south");
+         return true;
+
+      } else if (input.equals("west")){
+
+         System.out.println("I move west");
+         return true;
+
+      } else if (input.equals("quit")){
+
+         System.out.println("Bye!");
          return false;
 
       }
+
+      return false;
 
    }
 
