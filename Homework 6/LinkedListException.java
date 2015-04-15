2af095e610b35d9e6597b20c2fe35caaeab92af8
Harry Scells <harryscells@gmail.com>
2015-04-14 21:01:19 -0400

    comments

--- Homework 6/LinkedListException.java
+++ Homework 6/LinkedListException.java
@@ -1,3 +1,7 @@
+/**
+ * Give some super duper cool custom exception handelers to the LinkedList class
+ * @author Harry Scells
+ */
 public class LinkedListException extends Exception{
 
    private static final long serialVersionUID = 1337;
