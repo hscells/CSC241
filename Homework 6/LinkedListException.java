d34e44a1b310f68cfd904644b5ae2fd5b573f3ee
Harry Scells <harryscells@gmail.com>
2015-04-14 20:14:43 -0400

    added exception handler for linked lists

---
+++ Homework 6/LinkedListException.java
@@ -0,0 +1,11 @@
+public class LinkedListException extends Exception{
+
+   private static final long serialVersionUID = 1337;
+
+   public LinkedListException(String error){
+
+      super(error);
+
+   }
+
+}
